1194e92cb909415d6b1242c3f909ab52af2f141a
ltellll <145682209+ltellll@users.noreply.github.com>
2025-11-22 10:40:33 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,27 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+    1080zyku: {
+        api: 'https://api.1080zyku.com/inc/api_mac10.php',
+        name: 'TV-1080资源',
+    }
+    dyttzy: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: 'TV-电影天堂',
+    }
+    ruyi: {
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: 'TV-如意资源',
+    }
+    bfzy: {
+        api: 'https://bfzyapi.com/api.php/provide/vod',
+        name: 'TV-暴风资源',
+    }
+    wujin: {
+        api: 'https://api.wujinapi.me/api.php/provide/vod',
+        name: 'TV-无尽资源',
+    }
+    lzi: {
+        api: 'https://cj.lziapi.com/api.php/provide/vod',
+        name: 'TV-量子资源',
     }
 };
 
