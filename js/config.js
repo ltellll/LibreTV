9fd4acc7a632050fdb50d8c6d633639d2f5e7dbe
ltellll <145682209+ltellll@users.noreply.github.com>
2025-11-22 10:49:24 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -23,8 +23,8 @@ const SITE_CONFIG = {
 // API站点配置
 const API_SITES = {
     testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
+        api: 'https://api.1080zyku.com/inc/api_mac10.php',
+        name: 'TV-1080资源',
         adult: true
     }
     //ARCHIVE https://telegra.ph/APIs-08-12
